5373c22e150582b5e265cb8dd40005219c52f5bf
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2022-02-26 11:18:35 +0000

    passed POST readers and GET readers tests

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
-const { createReaderController } = require('./controllers/reader');
+const { createReaderController, readReadersController, getReaderById } = require('./controllers/reader');
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/readers', createReaderController);
+app.get('/readers', readReadersController);
+app.get('/readers/:id', getReaderById);
+// app.patch('/readers/:id', updateReader);
 
 module.exports = app;
--- src/controllers/reader.js
+++ src/controllers/reader.js
@@ -5,4 +5,22 @@ const createReaderController = async (req, res) => {
     res.status(201).json(newReader);
 };
 
-module.exports = { createReaderController };
+const readReadersController = async (req, res) => {
+    const readers = await Reader.findAll();
+    res.status(200).json(readers);
+};
+
+const getReaderById = async (req, res) => {
+    const readerId = req.params.id;
+    const reader = await Reader.findByPk(readerId);
+
+    if (!reader) {
+        res.status(404).json({ error: 'The reader could not be found.'});
+    } else {
+        res.status(200).json(reader);
+    }
+};
+
+// const updateReader = async (req, res) => {};
+
+module.exports = { createReaderController, readReadersController, getReaderById };
