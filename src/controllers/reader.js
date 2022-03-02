dd9af319a8c0d34d7b273087ad36551d1880c826
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2022-03-02 20:55:49 +0000

    refactored all controllers

--- src/controllers/reader.js
+++ src/controllers/reader.js
@@ -16,16 +16,7 @@ const getReaderById = (req, res) => getItemById(res, 'reader', req.params.id);
 
 const updateReader = (req, res) => updateItemById(res, 'reader', req.body, req.params.id)
 
-const deleteReader = async (req, res) => {
-    const readerId = req.params.id;
-    const deletedRows = await Reader.destroy({ where: { id: readerId } });
-
-    if (!deletedRows) {
-        res.status(404).json({ error: 'The reader could not be found.'});
-    } else {
-    res.status(204).send();
-    }
-};
+const deleteReader = (req, res) => deleteItemById(res, 'reader', req.params.id)
 
 module.exports = { 
     createReader, 
